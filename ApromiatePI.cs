aa8fa1e95c4edd87f6e11888dfa92e93ef0687dd
Darnae <108689727+Darnae@users.noreply.github.com>
2022-08-01 20:18:44 -0400

    Add files via upload

---
+++ ApromiatePI.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PI_Calculations
+{
+    public partial class ApromiatePI : Form
+    {
+        public ApromiatePI()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CalcuateButton_Click(object sender, EventArgs e)
+        {
+            Random terms = new Random();
+           
+            double PI = System.Math.PI * terms.NextDouble();
+
+            for (int i = 0; i < 10; i++)
+            {
+                TermsForPi.Text = terms.Next(1000,3000).ToString(); // will change range with each test
+
+                ReferenceForPi.Text = TermsForPi.Text.Trim() + " terms";
+
+                Results.Text = PI.ToString();
+
+            }
+        }
+    }
+}
